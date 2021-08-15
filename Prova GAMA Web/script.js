2cd8d13ae28b44f7cfbc73178012abe571965c05
Victor Bueno <vic_porsy@hotmail.com>
2021-08-15 13:38:40 -0300

    Update script.js
    Troca var por let e comentarios

--- Prova GAMA Web/script.js
+++ Prova GAMA Web/script.js
@@ -1,18 +1,18 @@
-function fat() {
-    var num = parseInt(document.getElementById("fat").value)
-    var fat
-    for (var i = 1; num >= 1; num--) {
-        i *= num;
+function fat() { // Função Fatorial
+    let num = parseInt(document.getElementById("fat").value)
+    let fat
+    for (let i = 1; num >= 1; num--) {
+        i *= num
         fat = i
     }
     return fat
 }
 
-function fibo() {
-    var num = parseInt(document.getElementById("fibo").value)
-    var soma, fib1 = 1,
+function fibo() { // Função Fibonacci
+    let num = parseInt(document.getElementById("fibo").value)
+    let soma, fib1 = 1,
         fib2 = 1
-    for (var i = 3; i <= num; i = i + 1) {
+    for (let i = 3; i <= num; i = i + 1) {
         soma = fib1 + fib2
         fib1 = fib2
         fib2 = soma
@@ -22,34 +22,34 @@ function fibo() {
 
 function soma() {
     soma = fat() + fibo()
-    if (fibo() == undefined) {
+    if (fibo() == undefined) { // Sem valor para fibo retorna apenas fat
         return fat()
-    } else if (fat() == undefined) {
+    } else if (fat() == undefined) { // Sem valor para fat retorna apenas fibo
         return fibo()
     } else
         return soma
 }
 
 function convert() {
-    if (fibo() == undefined) {
+    if (fibo() == undefined) { // Caso não declarado valor de fibo 
         num = fat()
-    } else if (fat() == undefined) {
+    } else if (fat() == undefined) { // Caso não declarado valor de fat
         num = fibo()
-    } else {
+    } else { // Caso os dois valores declarados
         num = soma
     }
     conv = num.toString()
-    if (conv.length % 2 != 0) {
+    if (conv.length % 2 != 0) { // Separar o valor em pares
         conv = 0 + conv
         conv1 = conv.match(/.{1,2}/g)
-        return conv1 //c679546q
+        return conv1
     } else {
         conv1 = conv.match(/.{1,2}/g)
         return conv1
     }
 }
 
-function enviar() {
+function enviar() { // Pop-up retornando os resultados armazenados nas variaveis
     r1 = fat()
     r2 = fibo()
     r3 = soma()
